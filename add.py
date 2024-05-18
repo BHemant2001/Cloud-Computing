37e0a9b11bd7a1af8ffed8248611c387d53e4e12
BHemant2001 <hemantbhamare2001@gmail.com>
2024-05-18 14:53:28 +0530

    add.py file modified

---
+++ add.py
@@ -0,0 +1,2 @@
+print("This is a test....merge conflict demo")
+second line added in local repository.
--- demo1.py
+++
@@ -1,6 +0,0 @@
-print("This is a Test")
-<<<<<<< HEAD
-print("This is edited in VI Editor")
-=======
-print("This is the update")
->>>>>>> b8883cfd05d47f4b36918f6c03e52f9669a62d99
